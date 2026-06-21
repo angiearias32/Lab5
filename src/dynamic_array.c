7e3424c598c0bafada6f0dfc143fca5783ea6b78
Angie Arias <angiearias32@gmail.com>
2026-06-20 23:47:02 -0600

    Implementacion de addElement para arreglo dinamico

--- src/dynamic_array.c
+++ src/dynamic_array.c
@@ -17,3 +17,19 @@ DynamicArray* createArray() {
 
 }
 
+void addElement(DynamicArray *array, int value) {
+
+    int *temp = realloc(array->data, (array->size + 1) * sizeof(int));
+
+    if (temp == NULL) {
+
+        return;
+
+    }
+
+    array->data = temp;
+
+    array->data[array->size] = value;
+
+    array->size++;
+}
