1e331b6bba4903a9c3a9365ab403c269d6d923f1
Angie Arias <angiearias32@gmail.com>
2026-06-24 22:33:34 -0600

    Arreglo dinamico completo

--- src/dynamic_array.c
+++ src/dynamic_array.c
@@ -33,3 +33,56 @@ void addElement(DynamicArray *array, int value) {
 
     array->size++;
 }
+
+void removeElement(DynamicArray *array, int index) {
+
+    if (index < 0 || index >= array->size) { // esta linea pregunta si es valido el valor
+        return;
+    }
+    for (int i = index; i < array->size - 1; i++) { // cierra el espacio que dejo el elemento eliminado
+
+        array->data[i] = array->data[i + 1];
+    }
+
+    array->size--; // actualiza el tamaño del arreglo
+
+    int *temp = realloc(array->data, array->size * sizeof(int)); // usamos temp para no perder la direccion del arreglo original
+
+    if (temp != NULL || array->size == 0) {
+
+        array->data = temp;
+
+    }
+}
+
+int getElement(DynamicArray *array, int index) {
+
+    if (index < 0 || index >= array->size) {
+
+        return -1;
+
+    }
+
+    return array->data[index];
+}
+
+
+void printArray(DynamicArray *array) {
+
+    for (int i = 0; i < array->size; i++) {
+
+        printf("%d ", array->data[i]);
+
+    }
+
+    printf("\n");
+
+}
+
+
+void freeArray(DynamicArray *array) {
+
+    free(array->data);
+    free(array);
+
+}
